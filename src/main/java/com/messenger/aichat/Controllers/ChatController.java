b42280a824748e4b1720902822fb5e067a41f6be
Dolar-Inflation <vandarkholm09@gmail.com>
2026-01-21 15:04:47 +0300

    добавил text to image и api для ext to 3d

--- src/main/java/com/messenger/aichat/Controllers/ChatController.java
+++ src/main/java/com/messenger/aichat/Controllers/ChatController.java
@@ -2,8 +2,10 @@ package com.messenger.aichat.Controllers;
 
 import com.messenger.aichat.Service.HuggingFaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +23,12 @@ public class ChatController {
         return huggingFaceService.sendPrompt(message);
     }
 
+    @PostMapping(
+            value = "/image",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public byte[] getImage(@RequestParam("prompt") String message) {
+        return huggingFaceService.sendImagePrompt(message);
+    }
 }
--- src/main/java/com/messenger/aichat/Service/HuggingFaceService.java
+++ src/main/java/com/messenger/aichat/Service/HuggingFaceService.java
@@ -1,12 +1,14 @@
 package com.messenger.aichat.Service;
 
 import com.messenger.aichat.Configuration.HuggingFaceConfig;
+import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
 
 
 import java.util.List;
@@ -18,11 +20,13 @@ public class HuggingFaceService {
     private final HuggingFaceConfig huggingFaceConfig;
     private final String apiKey;
     private final RestTemplate restTemplate;
+    private final String imageApiKey;
 
-    public HuggingFaceService(HuggingFaceConfig huggingFaceConfig, @Value("${huggingface.api.key}") String apiKey, RestTemplate restTemplate) {
+    public HuggingFaceService(HuggingFaceConfig huggingFaceConfig, @Value("${huggingface.api.key}") String apiKey,@Value("${stabilityai.api.key}") String imageApiKey, RestTemplate restTemplate) {
         this.huggingFaceConfig = huggingFaceConfig;
         this.apiKey=apiKey;
         this.restTemplate = restTemplate;
+        this.imageApiKey = imageApiKey;
     }
 
 
@@ -36,7 +40,7 @@ public class HuggingFaceService {
 
 
         Map<String,Object> body = Map.of(
-                "model", "meta-llama/Meta-Llama-3-70B-Instruct",
+                "model", "zai-org/GLM-Image",
                 "messages", List.of(Map.of("role", "user", "content", message) ),
                 "max_tokens", 200 );
 
@@ -44,5 +48,32 @@ public class HuggingFaceService {
 
         return restTemplate.postForObject(url, entity, String.class);
     }
+
+    public byte[] sendImagePrompt(String message){
+        String url = "https://api.stability.ai/v2beta/stable-image/generate/sd3";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(imageApiKey);
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.setAccept(List.of(MediaType.parseMediaType("image/*")));
+
+
+        MultiValueMap<String, Object> body =  new LinkedMultiValueMap<>();
+           body.add( "prompt", message);
+           body.add("output_format", "png");
+           body.add("aspect_ratio", "1:1" );
+
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+
+//        Map response = restTemplate.postForObject(url, entity, Map.class);
+
+
+//        String base64 = (String) response.get("image");
+        ResponseEntity<byte[]> response = restTemplate.exchange( url, HttpMethod.POST, entity, byte[].class );
+
+        return response.getBody();
+
+//        return restTemplate.postForObject(url, entity, byte[].class);
+    }
+
 
 }
