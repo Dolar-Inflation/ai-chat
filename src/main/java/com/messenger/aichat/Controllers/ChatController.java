72a4482e6f6111afc580ff323e0c3897e3347a8e
Dolar-Inflation <vandarkholm09@gmail.com>
2026-01-22 14:57:19 +0300

    добавил реализацию api для генерации 3d моделей по тексту, сгенерированная модель скачивается на докальный диск

--- src/main/java/com/messenger/aichat/Controllers/ChatController.java
+++ src/main/java/com/messenger/aichat/Controllers/ChatController.java
@@ -2,11 +2,12 @@ package com.messenger.aichat.Controllers;
 
 import com.messenger.aichat.Service.HuggingFaceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ChatController {
@@ -31,4 +32,22 @@ public class ChatController {
     public byte[] getImage(@RequestParam("prompt") String message) {
         return huggingFaceService.sendImagePrompt(message);
     }
+
+    @PostMapping(produces = "model/gltf-binary",value = "/3D")
+    public ResponseEntity<byte[]> send3DPrompt(@RequestParam("prompt") String message) {
+
+        try { byte[] glbBytes = huggingFaceService.send3DtextPrompt(message);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType("model/gltf-binary"));
+            headers.setContentDisposition( ContentDisposition.attachment() .filename("model.glb") .build() );
+            return new ResponseEntity<>(glbBytes, headers, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR) .body(null);
+        }
+
+
+    }
+
 }
--- src/main/java/com/messenger/aichat/Service/HuggingFaceService.java
+++ src/main/java/com/messenger/aichat/Service/HuggingFaceService.java
@@ -1,7 +1,6 @@
 package com.messenger.aichat.Service;
 
 import com.messenger.aichat.Configuration.HuggingFaceConfig;
-import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -10,7 +9,11 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Map;
 
@@ -21,8 +24,13 @@ public class HuggingFaceService {
     private final String apiKey;
     private final RestTemplate restTemplate;
     private final String imageApiKey;
+    private final String neural4dApiKey;
 
-    public HuggingFaceService(HuggingFaceConfig huggingFaceConfig, @Value("${huggingface.api.key}") String apiKey,@Value("${stabilityai.api.key}") String imageApiKey, RestTemplate restTemplate) {
+    public HuggingFaceService(HuggingFaceConfig huggingFaceConfig, @Value("${huggingface.api.key}") String apiKey,
+                              @Value("${stabilityai.api.key}") String imageApiKey,
+                              RestTemplate restTemplate,@Value("${neural4d.api.key}")
+                                  String neural4dApiKey) {
+        this.neural4dApiKey = neural4dApiKey;
         this.huggingFaceConfig = huggingFaceConfig;
         this.apiKey=apiKey;
         this.restTemplate = restTemplate;
@@ -73,6 +81,145 @@ public class HuggingFaceService {
         return response.getBody();
 
 //        return restTemplate.postForObject(url, entity, byte[].class);
+    }
+//    public byte[] send3DtextPrompt(String message) throws InterruptedException {
+//
+//        String url="https://api.neural4d.com/v1/tasks/text-to-3d";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setBearerAuth(neural4dApiKey);
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+//
+//        Map<String,Object> body = Map.of(
+//                "prompt",message,
+//                "version","premium-v1",
+//                "faceLimit",50000);
+//        HttpEntity<Map<String,Object>> entity = new HttpEntity<>(body, headers);
+//        Map<String,Object> response = restTemplate.exchange(url, HttpMethod.POST, entity, Map.class).getBody();
+//        String task_ID = (String) response.get("id");
+//
+//        String taskUrl = "https://api.neural4d.com/v1/tasks/"+task_ID;
+//
+//        ResponseEntity<Map> StatusResponse = restTemplate.exchange(taskUrl,HttpMethod.GET,entity, Map.class);
+//
+//        Map<String, Object> statusBody = StatusResponse.getBody();
+//        String state = (String) statusBody.get("status");
+//        while (true){
+//
+//            if ("completed".equalsIgnoreCase(state)){
+//                String downloadUrl = (String) statusBody.get("downloadUrl");
+//                return download3DModel(downloadUrl);
+//
+//            }
+//            if ("failed".equalsIgnoreCase(state)) {
+//                throw new IllegalStateException("Neural4D task failed: " + task_ID);
+//            }
+//
+//            Thread.sleep(3000);
+//
+//
+//        }
+
+
+//    }
+
+    public byte[] send3DtextPrompt(String message) throws InterruptedException, IOException {
+
+
+        String createUrl = "https://alb.neural4d.com:3000/api/generateModelWithText";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(neural4dApiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        Map<String, Object> body = Map.of(
+                "prompt", message,
+                "modelCount", 1,
+                "disablePbr", 0
+        );
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        Map<String, Object> response =
+                restTemplate.exchange(createUrl, HttpMethod.POST, entity, Map.class).getBody();
+
+        if (response == null || !response.containsKey("uuids")) {
+            throw new IllegalStateException("Neural4D did not return UUID");
+        }
+
+        List<String> uuids = (List<String>) response.get("uuids");
+        String uuid = uuids.get(0);
+
+
+        String retrieveUrl = "https://alb.neural4d.com:3000/api/retrieveModel";
+
+        Map<String, Object> retrieveBody = Map.of("uuid", uuid);
+        HttpEntity<Map<String, Object>> retrieveEntity = new HttpEntity<>(retrieveBody, headers);
+
+        while (true) {
+
+            Map<String, Object> retrieveResponse =
+                    restTemplate.exchange(retrieveUrl, HttpMethod.POST, retrieveEntity, Map.class).getBody();
+
+            if (retrieveResponse == null) {
+                throw new IllegalStateException("Neural4D returned empty retrieveModel response");
+            }
+
+            Integer codeStatus = (Integer) retrieveResponse.get("codeStatus");
+
+            if (codeStatus == 0) {
+
+                String modelUrl = (String) retrieveResponse.get("modelUrl");
+                byte[] glbBytes = download3DModel(modelUrl);
+
+                Path saved = SaveFile(glbBytes, "model.glb", "generated_models");
+
+                System.out.println("Saved to: " + saved.toAbsolutePath());
+
+                return download3DModel(modelUrl);
+            }
+
+            if (codeStatus == -1) {
+                throw new IllegalStateException("Neural4D token invalid or expired");
+            }
+
+            if (codeStatus == -2) {
+                throw new IllegalStateException("UUID does not exist: " + uuid);
+            }
+
+            if (codeStatus == -3) {
+                throw new IllegalStateException("Model generation failed: " + uuid);
+            }
+
+
+            Thread.sleep(3000);
+        }
+    }
+
+    public Path SaveFile(byte[] data , String fileName, String folderName) throws IOException {
+
+        Path path = Paths.get(folderName);
+        if (!Files.exists(path)) {
+            Files.createDirectories(path);
+        }
+        Path filepath = path.resolve(fileName);
+        Files.write(filepath, data, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        return filepath;
+
+    }
+
+
+
+
+
+    public byte[] download3DModel(String downloadUrl){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(neural4dApiKey);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        ResponseEntity <byte[]> response = restTemplate.exchange(downloadUrl, HttpMethod.GET, entity, byte[].class);
+        return response.getBody();
     }
 
 
